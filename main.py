3f21d1fdcd094dd63db127dcf6606ec87b0d5e00
gene891212 <gene891212@gmail.com>
2021-01-18 21:59:20 +0800

    finish output_data fuction

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import calendar
+import calendar, itertools, time
 import numpy as np
-import itertools
 from segment_code import NUMBER_COMMON_CATHODE
 
 class SmartCalendar():
     calendar.setfirstweekday(calendar.SUNDAY)
-    all_decoder_data = list(itertools.product([0, 1], repeat=3))
+    seg_decoder_data = list(itertools.product([0, 1], repeat=7))[:71]
 
     def get_time(self):
         now_time = time.localtime(time.time())
@@ -25,26 +24,27 @@ class SmartCalendar():
         else:
             return date
         return date
-    
-    def output(self):
-        for index, number in enumerate(display_date):
-            seg = NUMBER_COMMON_CATHODE[number]
-            decoder_data = all_decoder_data[index]
-            print(decoder_data, seg, NUMBER_COMMON_CATHODE.index(seg))
 
     def analyze_date(self):
         date = self.generate_calendar()
-        self.display_number = np.array([])
-        print(self.display_number)
+        display_number = []
         for week in date:
             for day in week:
                 ten = day // 10
                 one = day % 10
-                self.display_number += [[ten, one]]
-                # self.display_number = np.append(self.display_number, np.array([ten, one], ndmin=2))
-        print(self.display_number)
-        print(type(self.display_number))
+                if ten == 0 and one == 0:
+                    display_number += [10, 10]
+                else:
+                    display_number += [ten, one]
+        return display_number
+
+    def output_date(self):
+        display_number = self.analyze_date()
+        for index, number in enumerate(display_number):
+            seg = NUMBER_COMMON_CATHODE[number]
+            decoder_data = self.seg_decoder_data[index]
+            print('decoder: {} seg: {} {}'.format(decoder_data, seg, NUMBER_COMMON_CATHODE.index(seg)))
 
 if __name__ == "__main__":
     test = SmartCalendar()
-    test.analyze_date()
+    test.output_date()
