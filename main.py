9d659bf033f07d22c8d43ea7749c5e37e64d580c
gene_hsieh <gene_hsieh@gmail.com>
2021-01-18 15:00:24 +0800

    add output_data function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import calendar
 import numpy as np
+import itertools
+from segment_code import NUMBER_COMMON_CATHODE
 
-class SmartCalendar(calendar.Calendar):
+class SmartCalendar():
     calendar.setfirstweekday(calendar.SUNDAY)
+    all_decoder_data = list(itertools.product([0, 1], repeat=3))
+
     def generate_calendar(self, year, month):
         calendar1 = calendar.monthcalendar(year, month)
         date = np.array(calendar1)
@@ -15,6 +19,12 @@ class SmartCalendar(calendar.Calendar):
         else:
             return date
         return date
+    
+    def output(self):
+        for index, number in enumerate(display_date):
+            seg = NUMBER_COMMON_CATHODE[number]
+            decoder_data = all_decoder_data[index]
+            print(decoder_data, seg, NUMBER_COMMON_CATHODE.index(seg))
 
 if __name__ == "__main__":
     test = SmartCalendar()
